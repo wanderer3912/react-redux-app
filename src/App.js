cb0a8e4450cfc65847955cf300de06bb4b2040d6
wanderer3912 <komal3912@gmail.com>
2021-06-20 21:44:54 -0400

    crud operations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
+import NavBar from "./components/layout/NavBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-   <>
-  <BrowserRouter>
-        {/* <ToastContainer /> */}
-        {/* <NavBar /> */}
+    <>
+      <BrowserRouter>
+        <ToastContainer />
+        <NavBar />
         <Switch>
           <Route path="/signin" component={SignIn} />
           <Route path="/signup" component={SignUp} />
           <Route path="/" component={Dashboard} />
         </Switch>
       </BrowserRouter>
-      </>
+    </>
   );
 }
 
